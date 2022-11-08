7fdcf80c88c98556297d3d60b43123b07f9ce8de
Kenny Velasquez <kgvelasq@usc.edu>
2022-11-07 18:35:17 -0800

    "Got rid of geo location conversion and added map markers for events"

--- app/src/main/java/com/example/eventme/EventBox.java
+++ app/src/main/java/com/example/eventme/EventBox.java
@@ -12,11 +12,15 @@ import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
+import java.util.Collections;
+
 public class EventBox extends AppCompatActivity {
     String eventList[] = {"Cava Giveaway", "Nut Party", "Best Friend Party", "Adele Birthday Party", "Harvard How-tos", "Nice Greetings", "Physics Class", "Elon Musk Talk Show",  "Dinner With Lebron James", "Life of Pi Reading", "Temper Control", "Christmas Celebration", "Justin Bieber Meet and Greet", "Self Defense Class", "Love Yourself Readings", "Casper Party", "Standup Comedy", "Kevin Hart Party", "Bill Nye Exhibition", "Fire Exhibition"};
     String locationList[] = {"CAVA, Parking lot, South Hoover Street Suite 1840, Los Angeles, CA", "2809 Ellendale Place, Los Angeles, CA", "Insomnia Cookies, West Jefferson Boulevard, Los Angeles, CA", "1324 West 37th Place, Los Angeles, CA", "1261 W 36th Pl, Los Angeles, CA", "1259 W 36th Pl, Los Angeles, CA", "1261 W 36th Pl, Los Angeles, CA","1260 W 36th Pl, Los Angeles, CA","1263 W 36th Pl, Los Angeles, CA","1265 W 36th Pl, Los Angeles, CA", "CAVA, Parking lot, South Hoover Street Suite 1840, Los Angeles, CA", "2809 Ellendale Place, Los Angeles, CA", "Insomnia Cookies, West Jefferson Boulevard, Los Angeles, CA", "1324 West 37th Place, Los Angeles, CA", "1261 W 36th Pl, Los Angeles, CA", "1259 W 36th Pl, Los Angeles, CA", "1261 W 36th Pl, Los Angeles, CA","1260 W 36th Pl, Los Angeles, CA","1263 W 36th Pl, Los Angeles, CA","1265 W 36th Pl, Los Angeles, CA", "CAVA, Parking lot, South Hoover Street Suite 1840, Los Angeles, CA", "2809 Ellendale Place, Los Angeles, CA", "Insomnia Cookies, West Jefferson Boulevard, Los Angeles, CA", "1324 West 37th Place, Los Angeles, CA", "1261 W 36th Pl, Los Angeles, CA", "1259 W 36th Pl, Los Angeles, CA", "1261 W 36th Pl, Los Angeles, CA","1260 W 36th Pl, Los Angeles, CA","1263 W 36th Pl, Los Angeles, CA","1265 W 36th Pl, Los Angeles, CA"};
     String dateTimeList[] = {"9:30PM, 11/01/2022", "9:10PM, 11/03/2022", "7:00PM, 10/06/2022", "11:00PM, 11/15/2022", "9:30PM, 11/20/2022","9:35PM, 12/20/2022","10:00AM, 11/20/2022","7:15AM, 12/02/2022","7:30PM, 11/20/2022","10:30PM, 11/20/2022", "9:30PM, 11/01/2022", "9:10PM, 11/03/2022", "7:00PM, 10/06/2022", "11:00PM, 11/15/2022", "9:30PM, 11/20/2022","9:35PM, 12/20/2022","10:00AM, 11/20/2022","7:15AM, 12/02/2022","7:30PM, 11/20/2022","10:30PM, 11/20/2022"};
     String organizationList[] = {"Cava", "Deez", "Sausage Inc.", "ESPN", "Stephen A. Smith", "Jojo Siwa", "Marco Paolieri", "Tesla", "NBA", "Penguin House", "USC", "USC", "USC Village", "Fun Times Inc.", "Life is Good Inc.", "OVO", "LuLu Lemon", "Riot Games", "LAPD", "Crypto.com"};
+    double longitudeList[] = {-118.2845264, -118.2893416, -118.2853451, -118.2982277, -118.2964867, -118.296445, -118.2964867, -118.2967404, -118.2965932, -118.2966258, -118.2845264, -118.2893416, -118.2853451, -118.2982277, -118.2964867, -118.296445, -118.2964867, -118.2967404, -118.2965932, -118.2966258};
+    double latitudeList[] = {34.0250757, 34.0292234, 34.0250132, 34.0197438, 34.0220969, 34.022148, 34.0220969, 34.0216126, 34.0221403, 34.0221588, 34.0250757, 34.0292234, 34.0250132, 34.0197438, 34.0220969, 34.022148, 34.0220969, 34.0216126, 34.0221403, 34.0221588};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -103,6 +107,19 @@ public class EventBox extends AppCompatActivity {
         }
     }
 
+    public String[] getEventList() {
+        return eventList;
+    }
 
+    public String[] getLocationList() {
+        return locationList;
+    }
 
+    public double[] getLatitudeList() {
+        return latitudeList;
+    }
+
+    public double[] getLongitudeList() {
+        return longitudeList;
+    }
 }
--- app/src/main/java/com/example/eventme/Map.java
+++ app/src/main/java/com/example/eventme/Map.java
@@ -1,5 +1,6 @@
 package com.example.eventme;
 
+
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.location.Address;
@@ -9,40 +10,33 @@ import android.os.Bundle;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+
 import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.maps.CameraUpdate;
-import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+
+
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Iterator;
 import java.util.List;
-import java.util.ListIterator;
 
 public class Map extends AppCompatActivity implements OnMapReadyCallback {
 
-    private GoogleMap mGoogleMap;
+    String[] eventNames; //Placeholder for event information we will have later
+    String[] addresses;
+    double[] latitudeList;
+    double[] longitudeList;
 
-    private double longitude = 0.0;
-    private double latitude = 0.0;
-
-    List<List<Address>> geoCoderList;
-    List<String> eventNames; //Placeholder for event information we will have later
+    EventBox eventBox = new EventBox();
 
     private static final int LOCATION_PERMISSION_CODE = 101;
-
-    FusedLocationProviderClient fusedLocationProviderClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,47 +46,36 @@ public class Map extends AppCompatActivity implements OnMapReadyCallback {
 
         requestUserLocationPermission();
 
-        if(isUserLocationPermissionGranted()) {
-            SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.maps);
-            mapFragment.getMapAsync(this);
-        }
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.maps);
+        mapFragment.getMapAsync(this);
     }
 
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
-        mGoogleMap = googleMap;
-
-        geoCoderList = new ArrayList<>();
-        eventNames = new ArrayList<>();
-
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            mGoogleMap.setMyLocationEnabled(true);
-        }
-        mGoogleMap.getUiSettings().setZoomControlsEnabled(true);
-        mGoogleMap.getUiSettings().setZoomGesturesEnabled(true);
-
-        // Make geolocations for event addresses and add event names to list
-        try {
-            geoCoderList.add(new Geocoder(this).getFromLocationName("University of Southern California, Los Angeles, CA", 1));
-            eventNames.add("USC");
-            geoCoderList.add(new Geocoder(this).getFromLocationName("2813 Elendale Place, Los Angeles, CA", 1));
-            eventNames.add("Where Eric and Aristo live");
-        }
-        catch(Exception e) {
-            e.printStackTrace();
+            googleMap.setMyLocationEnabled(true);
         }
 
-        if(!geoCoderList.isEmpty()) {
-            for(int i = 0; i < geoCoderList.size(); i++) {
-                latitude = geoCoderList.get(i).get(0).getLatitude();
-                longitude = geoCoderList.get(i).get(0).getLongitude();
+        googleMap.getUiSettings().setZoomControlsEnabled(true);
+        googleMap.getUiSettings().setZoomGesturesEnabled(true);
 
-                Log.i("GOOGLE_MAP_TAG" + i, "Address has longitude: " + longitude + " latitude: " + latitude);
+        eventNames = new String[20];
+        addresses = new String[20];
+        latitudeList = new double[20];
+        longitudeList = new double[20];
 
-                LatLng event = new LatLng(latitude, longitude);
-                mGoogleMap.addMarker(new MarkerOptions().position(event).title(eventNames.get(i))); // String could be event name instead of USC
-                mGoogleMap.moveCamera(CameraUpdateFactory.newLatLng(event));
-            }
+        eventNames = eventBox.getEventList();
+        addresses = eventBox.getLocationList();
+        latitudeList = eventBox.getLatitudeList();
+        longitudeList = eventBox.getLongitudeList();
+
+        for(int i = 0; i < eventNames.length; i++) {
+            double latitude = latitudeList[i];
+            double longitude = longitudeList[i];
+            String eventName = eventNames[i];
+            String address = addresses[i];
+            LatLng event = new LatLng(latitude, longitude);
+            googleMap.addMarker(new MarkerOptions().position(event).title(eventName).snippet(address));
         }
     }
 
