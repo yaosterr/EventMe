7799dd8a80e8976c4e47268bb380df594fd62d71
Eric Yao <emyao@usc.edu>
2022-11-24 02:38:04 -0800

    Added Sorting for Registered Users

--- app/src/main/java/com/example/eventme/EventDate.java
+++ app/src/main/java/com/example/eventme/EventDate.java
@@ -22,13 +22,13 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.*;
 
-public class EventBox extends AppCompatActivity {
+public class EventDate extends AppCompatActivity {
 
     ArrayList<Event> total = new ArrayList<Event>();
 
-    String eventList[] = {"Cava Giveaway", "Nut Party", "Best Friend Party", "Adele Birthday Party", "Harvard How-tos", "Nice Greetings", "Physics Class", "Elon Musk Talk Show",  "Dinner With Lebron James", "Life of Pi Reading", "Temper Control", "Christmas Celebration", "Justin Bieber Meet and Greet", "Self Defense Class", "Love Yourself Readings", "Casper Party", "Standup Comedy", "Kevin Hart Party", "Bill Nye Exhibition", "Fire Exhibition"};
+    String eventList[] = {"Fire Exhibition", "Elon Musk Talk Show", "Dinner With Lebron James", "Best Friend Party", "Cava Giveaway", "Justin Bieber Meet and Greet", "Nut Party",  "Adele Birthday Party", "Harvard How-tos", "Nice Greetings", "Temper Control", "Physics Class", "Life of Pi Reading", "Christmas Celebration", "Casper Party", "Self Defense Class", "Bill Nye Exhibition", "Love Yourself Readings", "Standup Comedy", "Kevin Hart Party"};
     String locationList[] = {"CAVA, Parking lot, South Hoover Street Suite 1840, Los Angeles, CA", "2809 Ellendale Place, Los Angeles, CA", "Insomnia Cookies, West Jefferson Boulevard, Los Angeles, CA", "1324 West 37th Place, Los Angeles, CA", "1261 W 36th Pl, Los Angeles, CA", "1259 W 36th Pl, Los Angeles, CA", "1261 W 36th Pl, Los Angeles, CA","1260 W 36th Pl, Los Angeles, CA","1263 W 36th Pl, Los Angeles, CA","1265 W 36th Pl, Los Angeles, CA", "CAVA, Parking lot, South Hoover Street Suite 1840, Los Angeles, CA", "2809 Ellendale Place, Los Angeles, CA", "Insomnia Cookies, West Jefferson Boulevard, Los Angeles, CA", "1324 West 37th Place, Los Angeles, CA", "1261 W 36th Pl, Los Angeles, CA", "1259 W 36th Pl, Los Angeles, CA", "1261 W 36th Pl, Los Angeles, CA","1260 W 36th Pl, Los Angeles, CA","1263 W 36th Pl, Los Angeles, CA","1265 W 36th Pl, Los Angeles, CA", "CAVA, Parking lot, South Hoover Street Suite 1840, Los Angeles, CA", "2809 Ellendale Place, Los Angeles, CA", "Insomnia Cookies, West Jefferson Boulevard, Los Angeles, CA", "1324 West 37th Place, Los Angeles, CA", "1261 W 36th Pl, Los Angeles, CA", "1259 W 36th Pl, Los Angeles, CA", "1261 W 36th Pl, Los Angeles, CA","1260 W 36th Pl, Los Angeles, CA","1263 W 36th Pl, Los Angeles, CA","1265 W 36th Pl, Los Angeles, CA"};
-    String dateTimeList[] = {"9:30PM, 11/01/2022", "9:10PM, 11/03/2022", "7:00PM, 10/06/2022", "11:00PM, 11/15/2022", "9:30PM, 11/20/2022","9:35PM, 12/20/2022","10:00AM, 11/20/2022","7:15AM, 12/02/2022","7:30PM, 11/20/2022","10:30PM, 11/20/2022", "9:30PM, 11/01/2022", "9:10PM, 11/03/2022", "7:00PM, 10/06/2022", "11:00PM, 11/15/2022", "9:30PM, 11/20/2022","9:35PM, 12/20/2022","10:00AM, 11/20/2022","7:15AM, 12/02/2022","7:30PM, 11/20/2022","10:30PM, 11/20/2022"};
+    String dateTimeList[] = {"7:00PM, 10/06/2022", "7:00PM, 10/06/2022", "9:30PM, 11/01/2022", "9:30PM, 11/01/2022", "9:10PM, 11/03/2022", "9:10PM, 11/03/2022", "11:00PM, 11/15/2022", "11:00PM, 11/15/2022", "10:00AM, 11/20/2022", "10:00AM, 11/20/2022", "7:30PM, 11/20/2022", "7:30PM, 11/20/2022", "9:30PM, 11/20/2022", "9:30PM, 11/20/2022", "10:30PM, 11/20/2022", "10:30PM, 11/20/2022", "7:15AM, 12/02/2022", "9:35PM, 12/20/2022", "7:15AM, 12/02/2022", "9:35PM, 12/20/2022"};
     String organizationList[] = {"Cava", "Deez", "Sausage Inc.", "ESPN", "Stephen A. Smith", "Jojo Siwa", "Marco Paolieri", "Tesla", "NBA", "Penguin House", "USC", "USC", "USC Village", "Fun Times Inc.", "Life is Good Inc.", "OVO", "LuLu Lemon", "Riot Games", "LAPD", "Crypto.com"};
     double longitudeList[] = {-118.2845264, -118.2893416, -118.2853451, -118.2982277, -118.2964867, -118.296445, -118.2964867, -118.2967404, -118.2965932, -118.2966258, -118.2845264, -118.2893416, -118.2853451, -118.2982277, -118.2964867, -118.296445, -118.2964867, -118.2967404, -118.2965932, -118.2966258};
     double latitudeList[] = {34.0250757, 34.0292234, 34.0250132, 34.0197438, 34.0220969, 34.022148, 34.0220969, 34.0216126, 34.0221403, 34.0221588, 34.0250757, 34.0292234, 34.0250132, 34.0197438, 34.0220969, 34.022148, 34.0220969, 34.0216126, 34.0221403, 34.0221588};
@@ -76,11 +76,11 @@ public class EventBox extends AppCompatActivity {
             register.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    Toast.makeText(EventBox.this, "You have signed up. See you then!",
+                    Toast.makeText(EventDate.this, "You have signed up. See you then!",
                             Toast.LENGTH_LONG).show();
                     Event temp = new Event(eventList[finalI], locationList[finalI], dateTimeList[finalI],organizationList[finalI]);
                     total.add(temp);
-                    Intent minor = new Intent(EventBox.this, PersonalVoice.class);
+                    Intent minor = new Intent(EventDate.this, PersonalVoice.class);
 //                    Intent minor = new Intent(view.getContext(), PersonalVoice.class);
                     minor.putExtra("msg", total);
                     startActivity(minor);
@@ -139,39 +139,5 @@ public class EventBox extends AppCompatActivity {
 
     public double[] getLongitudeList() {
         return longitudeList;
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.common_menu_guest, menu);
-        return super.onCreateOptionsMenu(menu);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        int id = item.getItemId();
-
-        if (id == R.id.menu_refresh) {
-            startActivity(getIntent());
-            finish();
-            overridePendingTransition(0, 0);
-        } else if (id == R.id.menu_alphabetical) {
-            Intent intent = new Intent(EventBox.this, EventAlphabetical.class);
-            startActivity(intent);
-
-        } else if (id == R.id.menu_date) {
-            Intent intent = new Intent(EventBox.this, EventDate.class);
-            startActivity(intent);
-
-        }else if (id == R.id.menu_distance) {
-            Intent intent = new Intent(EventBox.this, EventDistance.class);
-            startActivity(intent);
-        }  else if (id == R.id.menu_cost) {
-            Intent intent = new Intent(EventBox.this, EventCost.class);
-            startActivity(intent);
-        } else {
-            Toast.makeText(EventBox.this, "Something went wrong", Toast.LENGTH_LONG).show();
-        }
-        return super.onOptionsItemSelected(item);
     }
 }
